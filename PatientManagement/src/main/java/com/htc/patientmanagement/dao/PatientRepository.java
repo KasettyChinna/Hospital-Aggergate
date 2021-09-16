71e101d61f2e2282c6a493e431a2df456fd81aaf
KasettyChinna <narsi1612@gmail.com>
2021-09-16 15:00:34 +0530

    first commit

--- PatientManagement/src/main/java/com/htc/patientmanagement/dao/PatientRepository.java
+++ PatientManagement/src/main/java/com/htc/patientmanagement/dao/PatientRepository.java
@@ -7,12 +7,12 @@ import com.htc.patientmanagement.model.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long>{
 
-	@Query(value = "SELECT * FROM patient WHERE first_name=?1",nativeQuery = true)
+	@Query(value = "SELECT patient_id,disease,first_name,last_name,mobile FROM patient WHERE first_name=?1",nativeQuery = true)
 	public Patient findByFirst_name(String firstName);
 	
-	@Query(value = "SELECT * FROM patient WHERE last_name=?1",nativeQuery = true)
+	@Query(value = "SELECT patient_id,disease,first_name,last_name,mobile FROM patient WHERE last_name=?1",nativeQuery = true)
 	public Patient findByLast_name(String lastName);
 	
-	@Query(value = "SELECT * FROM patient WHERE mobile=?1",nativeQuery = true)
+	@Query(value = "SELECT patient_id,disease,first_name,last_name,mobile FROM patient WHERE mobile=?1",nativeQuery = true)
 	public Patient findByMobile(long mobile);
 }
